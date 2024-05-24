eaea1fe5800cc7bf9d97b07a0310c34a221db25f
Eduardo <Edos21@gmail.com>
2024-05-24 10:46:10 -0300

    adds functionality to buttons on stations (#3)

--- client/src/api/stations.api.js
+++ client/src/api/stations.api.js
@@ -5,3 +5,7 @@ const StationsApi = axios.create({
 });
 
 export const getAllStations = () => StationsApi.get("/");
+
+export const callNextPatient = async (stationId) => await StationsApi.post(`/${stationId}/next_patient/`)
+
+export const endPatientTurn = async (stationId) => await StationsApi.post(`/${stationId}/end_turn/`)
--- client/src/components/StationList.jsx
+++ client/src/components/StationList.jsx
@@ -4,18 +4,19 @@ import { StationCard } from "./StationCard";
 
 export function StationList() {
     const [stations, setStations] = useState([]);
+    const loadStations = async () => {
+        const res = await getAllStations();
+        setStations(res.data);
+    };
+
     useEffect(() => {
-        async function loadStations() {
-            const res = await getAllStations();
-            setStations(res.data);
-        }
         loadStations();
-    }, [])
+    }, []);
     return (
         <div className="row">
             {stations.map(station => (
                 <div key={station.id} className="col-md-4 mb-4">
-                    <StationCard station={station} />
+                    <StationCard station={station} onUpdate={loadStations}/>
                 </div>
             ))}
         </div>
